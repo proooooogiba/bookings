17050fdbc47f3fe4ca6f1a206fdb7f309f3244f9
Ivan Pogiba <pogibuskaivan@gmail.com>
2023-07-02 23:57:06 +0300

    Improved error handling

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
 	"github.com/proooooogiba/bookings/internal/config"
 	"github.com/proooooogiba/bookings/internal/handlers"
+	"github.com/proooooogiba/bookings/internal/helpers"
 	"github.com/proooooogiba/bookings/internal/models"
 	"github.com/proooooogiba/bookings/internal/render"
 )
@@ -18,6 +20,8 @@ const portNumber = ":8080"
 
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 func main() {
 	err := run()
@@ -44,6 +48,12 @@ func run() error {
 	// change this to  true when in production
 	app.InProduction = false
 
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -63,7 +73,7 @@ func run() error {
 
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
-	render.NewTemplates(&app)
-
+	helpers.NewHelpers(&app)
+	
 	return nil
 }
